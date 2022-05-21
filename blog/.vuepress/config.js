00013d729ca4e7785ff86aa2896b9611fe837542
changhengheng <321121653@qq.com>
2022-05-21 15:11:49 +0800

    增加评论功能

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -2,8 +2,8 @@ module.exports = {
     title: "chh's Blog", // Title for the site. This will be displayed in the navbar.
     base: '/vuePressBlog/',
     dest: './dist',
-    repo: 'https://github.com/changhengheng/vuePressBlog',
     theme: '@vuepress/theme-blog',
+    repo: 'https://github.com/changhengheng/vuePressBlog',
     themeConfig: {
         dateFormat: 'YYYY-MM-DD',
         nav: [
@@ -34,9 +34,9 @@ module.exports = {
                 id: 'informalEssay',
                 dirname: '_informalEssay',
                 path: '/informalessay/',
-                title:'博客',
+                title: '博客',
                 itemPermalink: '/informalessay/:year/:month/:day/:slug',
-                frontmatter:{ //Front matter for entry page.
+                frontmatter: { //Front matter for entry page.
                     tag: 'informalessay'
                 },
             },
@@ -44,45 +44,52 @@ module.exports = {
                 id: 'tasklibrary',
                 dirname: '_tasklibrary',
                 path: '/tasklibrary/',
-                title:'任务库',
+                title: '任务库',
                 itemPermalink: '/tasklibrary/:year/:month/:day/:slug'
             },
             {
                 id: 'inbox',
                 dirname: '_inbox',
                 path: '/inbox/',
-                title:'收集箱',
+                title: '收集箱',
                 itemPermalink: '/inbox/:year/:month/:day/:slug'
             },
             {
                 id: 'WorldlyWisdom',
                 dirname: '_WorldlyWisdom',
                 path: '/WorldlyWisdom/',
-                title:'普世智慧',
+                title: '普世智慧',
                 itemPermalink: '/WorldlyWisdom/:year/:month/:day/:slug'
             },
             {
                 id: 'AesopsFables',
                 dirname: '_AesopsFables',
                 path: '/AesopsFables/',
-                title:'伊索寓言',
+                title: '伊索寓言',
                 itemPermalink: '/AesopsFables/:year/:month/:day/:slug'
             },
             {
                 id: 'guide',
                 dirname: '_guide',
                 path: '/guide/',
-                title:'食用指南',
+                title: '食用指南',
                 itemPermalink: '/guide/:year/:month/:day/:slug'
             },
             {
                 id: 'RelearnBasicVocabulary',
                 dirname: '_RelearnBasicVocabulary',
                 path: '/RelearnBasicVocabulary/',
-                title:'重学基础词汇',
+                title: '重学基础词汇',
                 itemPermalink: '/RelearnBasicVocabulary/:year/:month/:day/:slug'
             },
         ],
         smoothScroll: true,
-    }
+        comment: {
+            service: 'vssue',
+            owner: 'changhengheng',
+            repo: 'https://github.com/changhengheng/vuePressBlog',
+            clientId: 'fda7b35eaf2ce20f243c',
+            clientSecret: 'f4a65983a07792307104a1b38c45773000abe3fe',
+        }
+    },
 }
---
+++ dist/assets/js/38.8b80e618.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{659:function(t,e,n){"use strict";n.r(e);var s=n(21),l=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[this._v("很直接的一个解释就是，这个观念对你有意义，然后你对它产生了兴趣。")])])}),[],!1,null,null,null);e.default=l.exports}}]);
